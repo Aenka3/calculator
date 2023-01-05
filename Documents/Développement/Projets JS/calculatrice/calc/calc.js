0411100cbf8ac061301d3386d07679f57871b746
Aenka <armandnaki@gmail.com>
2023-01-05 20:59:03 +0100

    1.0

---
+++ Documents/Développement/Projets JS/calculatrice/calc/calc.js
@@ -0,0 +1,73 @@
+var calc_vars = {"nombre1":null, "nombre2": null , "operation" : null, "operations":{}};
+/*Initialisation de la calculatrice*/
+function calc_init(){
+	var btnList = document.querySelectorAll(".calculator .btn");
+	for(i = 0; i < btnList.length ; ++i){
+		btnList[i].addEventListener("click", calc_btn_pressed);
+	}
+	
+	calc_vars.nombre1 = document.querySelector("#calc_resultat > .calc_nombre1");
+	calc_vars.operation = document.querySelector("#calc_resultat > .calc_op");
+	calc_vars.nombre2 = document.querySelector("#calc_resultat > .calc_nombre2");
+	
+	
+	calc_vars.operations['*'] = calc_f_mult;
+	calc_vars.operations['/'] = calc_f_div;
+	calc_vars.operations['-'] = calc_f_moins;
+	calc_vars.operations['+'] = calc_f_plus;
+	calc_vars.operations['Mod'] = calc_f_mod;
+	
+}
+function calc_btn_pressed(){
+	if(this.classList.contains("btn_number")){
+		var elem = calc_vars.operation.innerHTML == "" ? calc_vars.nombre1 : calc_vars.nombre2;
+		if(elem.innerHTML == "0")
+			elem.innerHTML = "";
+		elem.innerHTML += this.innerHTML;
+	}else if(this.classList.contains("btn_reset")){//Bouton clear
+	calc_reset();
+	}else if(this.classList.contains("btn_op")){// Une opération
+		calc_vars.operation.innerHTML = this.innerHTML;
+	}else if(this.classList.contains("btn_equal")){
+		calc_equal();
+	}else if(this.classList.contains("btn_dot")){
+		calc_dot();
+	}
+}
+function calc_dot(){
+	var elem = calc_vars.operation.innerHTML == "" ? calc_vars.nombre1 : calc_vars.nombre2;
+	if(elem.innerHTML.indexOf('.') == -1){
+		elem.innerHTML += ".";
+	}
+}
+function calc_equal(){
+		var nbr1 = parseFloat(calc_vars.nombre1.innerHTML);
+		var nbr2 = parseFloat(calc_vars.nombre2.innerHTML);
+		var op = calc_vars.operation.innerHTML;
+		if(nbr1 != NaN && nbr2 != NaN && op != ""){
+			calc_reset();
+			calc_vars.nombre1.innerHTML = calc_vars.operations[op](nbr1, nbr2);
+		}
+}
+
+function calc_reset(){
+	calc_vars.nombre1.innerHTML = "0";
+	calc_vars.operation.innerHTML = "";
+	calc_vars.nombre2.innerHTML = "";
+}
+/*Operation mathématique*/
+function calc_f_mult(op1, op2){
+	return op1 * op2;
+}
+function calc_f_div(op1, op2){
+	return op2 == 0? "DIV 0" : op1 / op2;
+}
+function calc_f_moins(op1, op2){
+	return op1 - op2;
+}
+function calc_f_plus(op1, op2){
+	return  op1 + op2;
+}
+function calc_f_mod(op1, op2){
+	return  op1 % op2;
+}
---
+++ Documents/Développement/Projets JS/calculatrice/js/index.js
@@ -0,0 +1,121 @@
+/*
+let calc = {"nombre1" : null, "nombre2" : null, "operation" : null, "operations":{}};
+
+let operators = document.getElementsByClassName("operator");
+let numbers = document.getElementsByClassName("number");
+let dot = document.getElementById("dot");
+let clear = document.getElementById("clear");
+let result = document.getElementById("result");
+
+for (let i = 0; i < operators.length; i++) {
+  operators[i].addEventListener("click", operator);
+}
+
+function operator(evenement) {
+  console.log(evenement.target);
+  const value = evenement.target.value;
+  document.forms.output.value += value;
+}
+
+for (let i = 0; i < numbers.length; i++) {
+  let num = numbers[i];
+  num.addEventListener("click", number);
+}
+
+function number (evenement) {
+  const value = evenement.target.value;
+  document.forms.output.value += value;
+}
+
+*/
+let calc = {
+  "nombre1": null,
+  "nombre2": null,
+  "operateur": "",
+  "operations": {},
+  "output": null,
+  "etat": 0//0 vierge, 1 operation en ecriture
+};
+function calc_init() {
+  var inputs = document.querySelectorAll(".calculator .btn");
+  for (i = 0; i < inputs.length; ++i) {
+    inputs[i].addEventListener("click", calc_clicked);
+  }
+  calc.output = document.getElementById('output');
+  calc.operations["+"] = calc_f_plus;
+  calc.operations["-"] = calc_f_moins;
+  calc.operations["*"] = calc_f_mult;
+  calc.operations["/"] = calc_f_div;
+}
+function calc_clicked() {
+  if (this.classList.contains("operator")) {
+    //C'est un operateur
+    if (calc.nombre1 != null && calc.operateur == "") {
+      calc.output.value += this.value;
+      calc.operateur = this.value;
+    }
+  } else
+    if (this.classList.contains("number")) {
+      if (calc.operateur == "") {
+        if (calc.nombre1 != null && calc.nombre1 != "0" && calc.etat == 1)
+          calc.nombre1 += this.value;
+        else {
+          calc_clear();
+          calc.nombre1 = this.value;
+          calc.etat = 1;
+        }
+        calc.output.value = calc.nombre1;
+      } else {
+        if (calc.nombre2 != null)
+          calc.nombre2 += this.value;
+        else calc.nombre2 = this.value;
+        calc.output.value += this.value;
+      }
+
+    } else if (this.classList.contains("dot")) {
+      if (calc.operateur == "") {
+        let str = calc.nombre1 + "";
+        if (str.indexOf(".") == -1) {
+          calc.nombre1 += ".";
+          calc.output.value = calc.nombre1;
+        }
+      } else {
+        let str = calc.nombre2 + "";
+        if (str.indexOf(".") == -1) {
+          calc.nombre2 += ".";
+          calc.output.value = calc.nombre1 + calc.operateur + calc.nombre2;
+        }
+      }
+    }
+    else if (this.classList.contains("clear")) {
+      calc_clear();
+    }
+    else if (this.classList.contains("result")) {
+
+      let result = calc.operations[calc.operateur]
+        (parseFloat(calc.nombre1), parseFloat(calc.nombre2));
+      calc_clear();
+      calc.output.value = result;
+      calc.nombre1 = result;
+    }
+}
+function calc_clear() {
+  calc.output.value = 0;
+  calc.nombre1 = null;
+  calc.nombre2 = null;
+  calc.operateur = "";
+  calc.etat = 0;
+}
+/*Operation mathématique*/
+function calc_f_mult(op1, op2) {
+  return op1 * op2;
+}
+function calc_f_div(op1, op2) {
+  return op2 == 0 ? "/!\\ DIV 0 /!\\" : op1 / op2;
+}
+function calc_f_moins(op1, op2) {
+  return op1 - op2;
+}
+function calc_f_plus(op1, op2) {
+  return op1 + op2;
+}
